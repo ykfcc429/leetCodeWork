a1f742f416fdd3e9d5f44ad143073fd370862980
ykfcc429 <1141154320@qq.com>
2020-08-08 19:35:53 +0800

    update

--- src/com/aFeng/相同的数.java
+++ src/com/aFeng/相同的数.java
@@ -67,15 +67,4 @@ public class 相同的数 {
 
 }
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+
