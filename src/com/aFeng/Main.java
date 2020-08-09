e2a9bf7ab8a4218377755cbca3875ce97357217b
ykfcc429 <1141154320@qq.com>
2020-08-09 16:16:43 +0800

    create

---
+++ src/com/aFeng/Main.java
@@ -0,0 +1,36 @@
+
+package com.aFeng;
+
+import redis.clients.jedis.Jedis;
+
+
+/**
+ * 一个简单的redis分布式锁
+ * 对比单机的优势在于,在多个消费者的请求下,能够保证redis里的数据恒统一
+ * 缺点是资源从排队变成了竞争,想要获得资源必须不断的请求,排队就无效了,谁能请求到就处理谁,这样对于用户来说是非常操蛋的
+ * 所以为什么集群中必须需要消息队列的,还是需要排队的,只是对比于单机应用,我们不再用synchronized来对线程排队,而是使用消息队列对请求进行排队
+ * 把队列放在了消费者集群的门口,这样就能保证用户只需发起单次请求,就能被处理到,而不是直接被分布式锁丢弃请求.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Lock lock = new Lock();
+        Thread thread = new Thread(lock);
+        Thread thread1 = new Thread(lock);
+        Thread thread2 = new Thread(lock);
+        thread.start();
+        thread1.start();
+        thread2.start();
+    }
+
+}
+class Lock implements Runnable{
+    public void run() {
+        Jedis jedis = new Jedis();
+        Long goods = jedis.setnx("goods", "0");
+        if (goods > 0) {
+            jedis.incr("myNum");
+        }
+        jedis.del("goods");
+    }
+}
---
+++ src/com/aFeng/复原IP地址.java
@@ -0,0 +1,82 @@
+package com.aFeng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
+ *
+ * 有效的 IP 地址正好由四个整数（每个整数位于 0 到 255 之间组成），整数之间用 '.' 分隔。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * 输入: "25525511135"
+ * 输出: ["255.255.11.135", "255.255.111.35"]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/restore-ip-addresses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 复原IP地址 {
+
+    static final int SEG_COUNT = 4;
+    static List<String> ans = new ArrayList<>();
+    static int[] segments = new int[SEG_COUNT];
+
+
+    public static void main(String[] args) {
+        List<String> strings = restoreIpAddresses("1921681104");
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    static List<String> restoreIpAddresses(String s) {
+        segments = new int[SEG_COUNT];
+        dfs(s, 0, 0);
+        return ans;
+    }
+
+    static void dfs(String s, int segId, int segStart) {
+        // 如果找到了 4 段 IP 地址并|且遍历完了字符串，那么就是一种答案
+        if (segId == SEG_COUNT) {
+            if (segStart == s.length()) {
+                StringBuilder ipAddr = new StringBuilder();
+                for (int i = 0; i < SEG_COUNT; ++i) {
+                    ipAddr.append(segments[i]);
+                    if (i != SEG_COUNT - 1) {
+                        ipAddr.append('.');
+                    }
+                }
+                ans.add(ipAddr.toString());
+            }
+            return;
+        }
+
+        // 如果还没有找到 4 段 IP 地址就已经遍历完了字符串，那么提前回溯
+        if (segStart == s.length()) {
+            return;
+        }
+
+        // 由于不能有前导零，如果当前数字为 0，那么这一段 IP 地址只能为 0
+        if (s.charAt(segStart) == '0') {
+            segments[segId] = 0;
+            dfs(s, segId + 1, segStart + 1);
+        }
+
+        // 一般情况，枚举每一种可能性并递归
+        int addr = 0;
+        for (int segEnd = segStart; segEnd < s.length(); ++segEnd) {
+            addr = addr * 10 + (s.charAt(segEnd) - '0');
+            if (addr > 0 && addr <= 0xFF) {
+                segments[segId] = addr;
+                dfs(s, segId + 1, segEnd + 1);
+            } else {
+                break;
+            }
+        }
+    }
+
+}
