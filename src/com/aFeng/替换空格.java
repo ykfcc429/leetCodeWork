ca12d3e50767f29ad71b1a22b76fb801dd5e13c4
ykfcc429 <1141154320@qq.com>
2020-11-09 14:58:10 +0800

    不孬

---
+++ src/com/aFeng/替换空格.java
@@ -0,0 +1,11 @@
+package com.aFeng;
+
+/**
+ * @author ykf
+ * @version 2020/11/9
+ */
+public class 替换空格 {
+    public String replaceSpace(String s) {
+        return s.replaceAll(" ","%20");
+    }
+}
