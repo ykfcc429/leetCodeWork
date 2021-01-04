cc7c9de9c1b2a32051f0645d179893004ea28f5f
ykfcc429 <1141154320@qq.com>
2021-01-04 10:29:36 +0800

    不孬

---
+++ src/com/aFeng/斐波那契数.java
@@ -0,0 +1,17 @@
+package com.aFeng;
+
+/**
+ * @author ykf
+ * @version 2021/1/4
+ */
+public class 斐波那契数 {
+    public int fib(int n) {
+        int[] arr = new int[n<2?2:n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+        return arr[n];
+    }
+}
