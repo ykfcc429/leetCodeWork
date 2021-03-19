215536842a610274be65eb5b661f2bb32dfad099
yankaifeng <yankaifeng@hzzl.net>
2021-03-19 10:20:11 +0800

    孬

---
+++ src/com/aFeng/设计停车系统.java
@@ -0,0 +1,24 @@
+package com.aFeng;
+
+/**
+ * @author ykf
+ * @version 2021/3/19
+ */
+public class 设计停车系统 {
+
+
+}
+
+class ParkingSystem{
+
+    private int[] nums;
+
+    public ParkingSystem(int big, int medium, int small) {
+        nums = new int[]{big,medium,small};
+    }
+
+    public boolean addCar(int carType) {
+        nums[carType-1]--;
+        return nums[carType-1]>-1;
+    }
+}
