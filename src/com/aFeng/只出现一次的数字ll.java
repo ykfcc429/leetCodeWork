40a358f7301d8f10259cd7e28970e2baafbcf3c2
ykfcc429 <1141154320@qq.com>
2021-04-30 09:34:48 +0800

    update

--- src/com/aFeng/只出现一次的数字ll.java
+++ src/com/aFeng/只出现一次的数字ll.java
@@ -10,17 +10,15 @@ import java.util.Map;
 public class 只出现一次的数字ll {
 
     public int singleNumber(int[] nums) {
-        int result = 0;
         Map<Integer,Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num,map.getOrDefault(num,0)+1);
         }
         for (Map.Entry<Integer, Integer> integerIntegerEntry : map.entrySet()) {
             if(integerIntegerEntry.getValue()==1) {
-                result = integerIntegerEntry.getKey();
-                break;
+                return integerIntegerEntry.getKey();
             }
         }
-        return result;
+        return 0;
     }
 }
