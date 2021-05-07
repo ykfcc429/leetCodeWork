ee61f4a3a713d27565792220d5a666b6cdc12ba7
ykfcc429 <1141154320@qq.com>
2021-05-07 09:24:20 +0800

    不孬

---
+++ src/com/aFeng/数组异或操作.java
@@ -0,0 +1,16 @@
+package com.aFeng;
+
+/**
+ * @author ykf
+ * @version 2021/5/7
+ */
+public class 数组异或操作 {
+
+    public int xorOperation(int n, int start) {
+        int tmp = start;
+        for (int i = 1; i < n; i++) {
+            tmp = (start + 2*i)^tmp;
+        }
+        return tmp;
+    }
+}
