826d90772e9d8bed2c20480ad70364e3faa5d5c4
ykfcc429 <1141154320@qq.com>
2021-05-07 09:34:48 +0800

    不孬

--- src/com/aFeng/数组异或操作.java
+++ src/com/aFeng/数组异或操作.java
@@ -9,7 +9,7 @@ public class 数组异或操作 {
     public int xorOperation(int n, int start) {
         int tmp = start;
         for (int i = 1; i < n; i++) {
-            tmp = (start + 2*i)^tmp;
+            tmp ^= start + 2*i;
         }
         return tmp;
     }
